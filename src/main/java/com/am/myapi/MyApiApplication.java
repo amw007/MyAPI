dc61b714640b6b8de4926a991654fb2f78929741
amw007 <mariesmw007@gmail.com>
2022-02-02 21:57:06 +0530

    builded

--- src/main/java/com/am/myapi/MyApiApplication.java
+++ src/main/java/com/am/myapi/MyApiApplication.java
@@ -2,7 +2,7 @@ package com.am.myapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
 public class MyApiApplication {
 
